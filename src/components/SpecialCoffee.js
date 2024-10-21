44c51ff93e16d5200e5c38a3b778670c0353120e
David Flores <davidafloresvel@gmail.com>
2024-10-21 14:39:37 -0600

    made some changes to the flig card to just switching the content from spanish to english

--- src/components/SpecialCoffee.js
+++ src/components/SpecialCoffee.js
@@ -1,13 +1,26 @@
-// src/components/SpecialCoffee.js
 import React, { useState } from 'react';
-import './SpecialCoffee.css'; // Import the CSS for flipping effect
-import Coffee from '../assets/cafe-de-olla.jpg'; // Import the coffee image
+import './SpecialCoffee.css';
+import Coffee from '../assets/cafe-de-olla.jpg';
+
+const coffeeDetails = {
+  es: {
+    title: "Café De Olla",
+    description: "Café de Olla es un café tradicional mexicano condimentado con canela, clavo, anís estrellado y endulzado con piloncillo. Hecho en una olla de barro mexicana o vasija de barro.",
+    languageToggle: "Click for English",
+  },
+  en: {
+    title: "Café De Olla",
+    description: "Café de Olla is a traditional Mexican coffee spiced with cinnamon, cloves, star anise, and sweetened with piloncillo. Made in a Mexican olla de barro, or clay pot.",
+    languageToggle: "Presiona para Español",
+  },
+  price: "$ 4.50",
+};
 
 const SpecialCoffee = () => {
   const [isFlipped, setIsFlipped] = useState(false);
 
-  // Toggle the flip state when clicked
   const handleClick = () => setIsFlipped(!isFlipped);
+  const language = isFlipped ? 'en' : 'es';
 
   return (
     <>
@@ -15,29 +28,15 @@ const SpecialCoffee = () => {
       <img src={Coffee} alt="Café de Olla" className="coffee-image" />
       <div className="special-coffee-container">
         <div className={`card ${isFlipped ? 'flipped' : ''}`} onClick={handleClick}>
-          {/* Front side with the description */}
-          <div className="card-front">
-            <h2>Café De Olla</h2>
-            <p>
-              Café de Olla es un café tradicional mexicano condimentado con
-              canela, clavo, anís estrellado y endulzado con piloncillo. Hecho
-              en una olla de barro mexicana o vasija de barro.
-            </p>
-            <p>"Click for English"</p>
-          </div>
-          {/* Back side with the description */}
-          <div className="card-back">
-            <h2>Café De Olla</h2>
-            <p>
-              Café de Olla is a traditional Mexican coffee spiced with cinnamon,
-              cloves, star anise, and sweetened with piloncillo. Made in a
-              Mexican olla de barro, or clay pot.
-            </p>
-            <p>"Presiona para Español"</p>
+          <div className="card-content">
+            <h2>{coffeeDetails[language].title}</h2>
+            <p>{coffeeDetails[language].description}</p>
+            <br />
+            <p className='price'>{coffeeDetails.price}</p>
+            <p>{coffeeDetails[language].languageToggle}</p>
           </div>
         </div>
       </div>
-      <p className="price">Café de olla $4.50</p>
     </>
   );
 };
